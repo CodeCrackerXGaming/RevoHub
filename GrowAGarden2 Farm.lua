db3262e5ed35cd1c8f5dbb4494859b4fc0c4d1f4
CodeCrackerXGaming <datfardcatyt@gmail.com>
2026-06-21 17:31:40 -0400

    Update GrowAGarden2 Farm.lua

--- GrowAGarden2 Farm.lua
+++ GrowAGarden2 Farm.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImUyYjUzZTQzLTc5N2QtNDEyYy05NDc2LTIyZjRlZjVhNmQ4ZSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJnYWcyX21haW5fdmlzdWFsIn19"))()
+ID="e2b53e43-797d-412c-9476-22f4ef5a6d8e";
+RUN_AFTER=loadstring(game:HttpGet("https://cdn.project-reverse.org/gag2_dupe.luau"));
+loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();
